89decdcf318cc167baae08ee0f3f8b62e526cd57
Pascal Bertschi <pascal.bertschi@hotmail.com>
2017-03-31 11:47:35 +0200

    feat: added slack notifications

--- src/app/models/rpc-call-request.model.ts
+++
@@ -1,4 +0,0 @@
-export interface RpcCallRequestModel {
-  correlationId: number;
-  args: any[];
-}
--- src/app/models/rpc-call-response.model.ts
+++
@@ -1,4 +0,0 @@
-export interface RpcCallResponseModel<TResult> {
-  correlationId: number;
-  result: TResult;
-}
